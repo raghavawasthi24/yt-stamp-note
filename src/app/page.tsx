9ec680472c0a8197fc96d3896d73518b25e49960
Raghav Awasthi <raghavawasthi240@gmail.com>
2024-05-18 12:34:41 +0530

    refactor: video player, title card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,44 @@
-"use client";
-import React, { useState, useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import React from "react";
 
-export default function Page() {
-  const [time, setTime] = useState(0);
-  const playerRef = useRef(null);
-
-  useEffect(() => {
-    // Load YouTube IFrame API script
-    const tag = document.createElement("script");
-    tag.src = "https://www.youtube.com/iframe_api";
-    const firstScriptTag = document.getElementsByTagName("script")[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    // Initialize YouTube Player
-    window.onYouTubeIframeAPIReady = () => {
-      playerRef.current = new window.YT.Player("player", {
-        height: "315",
-        width: "560",
-        videoId: "_mR6bY-ndso",
-        playerVars: {
-          playsinline: 1,
-        },
-        events: {
-          onReady: onPlayerReady,
-        },
-      });
-    };
-
-    function onPlayerReady(event) {
-      // Player is ready
-    }
-
-    return () => {
-      // Cleanup function to remove YouTube player instance
-      if (playerRef.current) {
-        playerRef.current.destroy();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    if (playerRef.current && playerRef.current.seekTo) {
-      playerRef.current.seekTo(time, true); // Seek to the specified time and play the video
-    }
-  }, [time]);
-
+export default function page() {
   return (
     <div>
-      <div id="player"></div>
-      <button onClick={() => setTime(50)}>Click me</button>
-      <button onClick={() => setTime(500)}>Click</button>
+      <h1>Video Player with Notes</h1>
+      <div className="px-8 flex flex-col gap-8">
+        <iframe
+          width="100%"
+          height="615"
+          src="https://www.youtube.com/embed/GmIsmQ0cHxg?si=8yi3yRS_ysyNacGL"
+          title="YouTube video player"
+          // frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          // referrerpolicy="strict-origin-when-cross-origin"
+          // allowfullscreen={true}
+        ></iframe>
+        <div className="flex flex-col gap-2 border-b-8">
+          <p className="text-lg font-semibold">Video title goes here</p>
+          <p>This is the description of the video</p>
+        </div>
+
+        <div>
+          <div className="flex flex-col gap-2">
+            <p className="text-lg font-semibold">Video title goes here</p>
+            <p>This is the description of the video</p>
+          </div>
+
+          <div>
+            <p>12 May ‘24</p>
+            <p>Timestamp: 01 min 30 sec</p>
+            <p>This is my first note.</p>
+
+            <div>
+              <Button>Delete</Button>
+              <Button>Edit</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
